7cae97c7c9f9aaf48a2f96ae5e051ce758556ca9
Emanuel <82622208+emjeisomUCL@users.noreply.github.com>
2021-06-20 15:20:34 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// O aluno deverá criar uma biblioteca para trabalhar com numeros complexos. 
+// Ela deverá ser composta por uma estrutura, a qual será dado o nome de tComplexos, 
+// que tera a forma de um numero complexo (parte real e parte imaginária).
+// A sua biblioteca deverá ter funções que executamas seguintes operações: 
+// soma, subtração, multiplicação, divisçao e modulo dos numeros complexos.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  
+  
+
+  cout<<endl<<endl;
+  return 0;
+}
