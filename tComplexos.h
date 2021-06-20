319ea7664457f4d43aafdf7344ed57e752d28c33
Emanuel <82622208+emjeisomUCL@users.noreply.github.com>
2021-06-20 15:39:01 +0000

    Criação da struct com os seus atributos

--- tComplexos.h
+++ tComplexos.h
@@ -3,6 +3,14 @@
 
 using namespace std;
 
-namespace numComplex{
+//Fiz o uso de namespace pois esta em um exemplo na documentação da Microsoft (https://docs.microsoft.com/pt-br/cpp/build/walkthrough-creating-and-using-a-static-library-cpp?view=msvc-160) mas usei como exemplo somente até essa porque lá esta sendo implementado classes e aqui vamos implementar struct
 
+namespace numComplex{
+  //Criação da struct com os seus atributos, ou seja, os dois numeros complexos que serão usados nas operações
+  struct tComplexos{
+    double pReal01;
+    double pImaginario01;
+    double pReal02;
+    double pImaginario02;
+  };
 }
