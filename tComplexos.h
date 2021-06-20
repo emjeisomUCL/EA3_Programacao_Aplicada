667d175a8dcfeac300e92cb21c55ea847d6cba28
Emanuel <82622208+emjeisomUCL@users.noreply.github.com>
2021-06-20 15:44:34 +0000

    Criado função para preencher os atributos de tComplexos

--- tComplexos.h
+++ tComplexos.h
@@ -3,7 +3,9 @@
 
 using namespace std;
 
-//Fiz o uso de namespace pois esta em um exemplo na documentação da Microsoft (https://docs.microsoft.com/pt-br/cpp/build/walkthrough-creating-and-using-a-static-library-cpp?view=msvc-160) mas usei como exemplo somente até essa porque lá esta sendo implementado classes e aqui vamos implementar struct
+// Fiz o uso de namespace pois esta em um exemplo na documentação da Microsoft 
+// (https://docs.microsoft.com/pt-br/cpp/build/walkthrough-creating-and-using-a-static-library-cpp?view=msvc-160) 
+// mas usei como exemplo somente até essa porque lá esta sendo implementado classes e aqui vamos implementar struct
 
 namespace numComplex{
   //Criação da struct com os seus atributos, ou seja, os dois numeros complexos que serão usados nas operações
@@ -12,5 +14,16 @@ namespace numComplex{
     double pImaginario01;
     double pReal02;
     double pImaginario02;
+
+    // Criado função para preencher os atributos de tComplexos
+    void enterNumber(double rNum1, double iNum1, double rNum2, double iNum2){
+      pReal01 = rNum1;
+      pImaginario01 = iNum1;
+      pReal02 = rNum2;
+      pImaginario02 = iNum2;
+    }
+
+    
+
   };
 }
